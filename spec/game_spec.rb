f0f4e216aa03991d3511fc65885bd15fd0048cc1
Emanuele DelBono <emanuele@codiceplastico.com>
2014-04-11 09:12:53 +0200

    first iteration a part of the second

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -13,10 +13,20 @@ describe Game do
     expect { Game.new(horse) }.to raise_error
   end
 
-    it "creates a game with more than 8 players fails" do
-      p = Player.new('test')
-      expect { Game.new(p,p,p,p,p,p,p,p,p) }.to raise_error
+  it "creates a game with more than 8 players fails" do
+    p = Player.new('test')
+    expect { Game.new(p,p,p,p,p,p,p,p,p) }.to raise_error
   end
 
   #Create a game with two players named Horse and Car. Within creating 100 games, both orders [Horse, Car] and [car, horse] occur.
+  it "creates a game with 2 players. Within 100 run both orders should occur" do
+    horse = Player.new('horse')
+    car = Player.new('car')
+    100.times do 
+      Game.new(horse, car)
+        car_horse &= Game.players == [car, horse]
+        horse_car &= Game.players == [horse, car]
+    end
+  end
+
 end
